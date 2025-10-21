d051ba5e48a642bc669332946468c2605a4b5996
Md Sarim Rashid Khan <sarim.khan@clapgrow.com>
2025-10-21 17:30:12 +0530

    i) Added duty type, request type, notes and other fields to trip request doctype
    ii) Added has multiple bookings and number of bookings to the extracted email type
    
    iii) Updated naming convention of trip request to include passenger name & date & time.

--- sarim_app/sarim_app/doctype/fc_btw_trip_requests/fc_btw_trip_requests.py
+++ sarim_app/sarim_app/doctype/fc_btw_trip_requests/fc_btw_trip_requests.py
@@ -1,9 +1,52 @@
-# Copyright (c) 2025, sarim and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class FC_BTW_Trip_Requests(Document):
-	pass
+	def autoname(self):
+		sender = self.passenger_name or "unknown"
+		date_obj = frappe.utils.get_datetime(self.pickup_date) if self.pickup_date else frappe.utils.now_datetime()
+		timestamp = date_obj.strftime("%d-%m-%Y_%I-%M %p")
+
+		base_name = f"{sender}_{timestamp}"
+		existing_count = frappe.db.count(
+			"FC_BTW_Trip_Requests",
+			{"name": ["like", f"{base_name}%"]}
+		)
+
+		self.name = base_name if existing_count == 0 else f"{base_name}_{existing_count + 1}"
+
+
+# import frappe
+# from frappe.model.document import Document
+
+# class FC_BTW_Trip_Requests(Document):
+#     def autoname(self):
+#         """
+#         Naming Convention:
+#         Use same as Extracted Email base name (sender_dd-mm-yyyy_HH-MM AM/PM)
+#         + suffix if multiple bookings exist.
+#         """
+
+#         # use the mail_link or ai_json_response to fetch the Extracted Email name
+#         base_name = None
+
+#         # If mail_link is set, fetch the linked extracted email
+#         if self.mail_link:
+#             extracted_email = frappe.db.get_value("FC_BTW_Extracted_Emails", self.mail_link, "name")
+#             if extracted_email:
+#                 base_name = extracted_email
+
+#         # fallback if no mail_link or email found
+#         if not base_name:
+#             base_name = "unknown_" + frappe.utils.now_datetime().strftime("%d-%m-%Y_%I-%M %p")
+
+#         # check if any trips exist with same base
+#         existing_count = frappe.db.count(
+#             "FC_BTW_Trip_Requests",
+#             {"name": ["like", f"{base_name}%"]}
+#         )
+
+#         if existing_count == 0:
+#             self.name = base_name
+#         else:
+#             self.name = f"{base_name}_{existing_count + 1}"
